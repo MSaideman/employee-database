3a095d16ee3e14f9db06a23e4b1099211b967aa2
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-08-27 14:18:08 -0400

    added functions for terminal application

--- server.js
+++ server.js
@@ -14,3 +14,74 @@ sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
 
+
+function startEmployeeManager() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Departments",
+          "View All Roles",
+          "Add Employee",
+          "Add Department",
+          "Add Role",
+          "Update Employee Role",
+          "Exit"
+        ]
+      })
+      .then(function(answer) {   
+  })};
+
+function viewAllEmployees() {
+    inquirer
+      .prompt({
+      })
+      .then(function(answer) {   
+})};
+
+function viewAllDepartments() {
+    inquirer
+      .prompt({
+      })
+      .then(function(answer) {   
+})};
+
+function viewAllRoles() {
+    inquirer
+      .prompt({
+      })
+      .then(function(answer) {   
+})};
+
+function addEmployee() {
+    inquirer
+      .prompt({
+      })
+      .then(function(answer) {   
+})};
+
+function addDepartment() {
+    inquirer
+      .prompt({
+      })
+      .then(function(answer) {   
+})};
+
+function addRole() {
+    inquirer
+      .prompt({
+      })
+      .then(function(answer) {   
+})};
+
+function updateEmployeeRole() {
+    inquirer
+      .prompt({
+      })
+      .then(function(answer) {   
+})};
+
+
