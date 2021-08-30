2aba40e1080f78c6ea5cb9542d886de2bcdd411a
MSaideman <82477037+MSaideman@users.noreply.github.com>
2021-08-30 16:30:27 -0400

    added cases to startemployeemanager function

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const inquirer = require("inquirer");
 const fs = require("fs");
-const sequelize = require('./config/connection');
-const cTable = require('console.table');
+const sequelize = require("./config/connection");
+const cTable = require("console.table");
 
-// call express 
+// call express
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,91 +12,97 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// route for express to initiate 
-app.get('/', function(req, res){
-  res.send('welcome to express');
+// route for express to initiate
+app.get("/", function (req, res) {
+  res.send("welcome to express");
 });
 
 // Force true to drop/recreate table(s) on every sync
 sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
 
-
 function startEmployeeManager() {
-    inquirer
-      .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-          "View All Employees",
-          "View All Departments",
-          "View All Roles",
-          "Add Employee",
-          "Add Department",
-          "Add Role",
-          "Update Employee Role",
-          "Exit"
-        ]
-      })
-      .then(function(answer) {   
-  })};
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Departments",
+        "View All Roles",
+        "Add Employee",
+        "Add Department",
+        "Add Role",
+        "Update Employee Role",
+        "Exit",
+      ],
+    })
+    .then(function (answer) {
+      switch (answer) {
+        case "View All Employees":
+          viewAllEmployees();
+          break;
+        case "View All Departments":
+          viewAllDepartments();
+          break;
+        case "View All Roles":
+          viewAllRoles();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+        case "Exit":
+          text = "Bye!";
+          break;
+        default:
+          text = "No value found";
+      }
+    });
+}
 
 function viewAllEmployees() {
-    inquirer
-      .prompt({
-      })
-      .then(function(answer) {   
-        startEmployeeManager()
-})};
+  startEmployeeManager();
+}
 
 function viewAllDepartments() {
-    inquirer
-      .prompt({
-      })
-      .then(function(answer) {   
-        startEmployeeManager()
-})};
+  startEmployeeManager();
+}
 
 function viewAllRoles() {
-    inquirer
-      .prompt({
-      })
-      .then(function(answer) {   
-        startEmployeeManager()
-})};
+  startEmployeeManager();
+}
 
 function addEmployee() {
-    inquirer
-      .prompt({
-      })
-      .then(function(answer) {  
-        startEmployeeManager() 
-})};
+  inquirer.prompt({}).then(function (answer) {
+    startEmployeeManager();
+  });
+}
 
 function addDepartment() {
-    inquirer
-      .prompt({
-      })
-      .then(function(answer) {  
-        startEmployeeManager() 
-})};
+  inquirer.prompt({}).then(function (answer) {
+    startEmployeeManager();
+  });
+}
 
 function addRole() {
-    inquirer
-      .prompt({
-      })
-      .then(function(answer) {   
-        startEmployeeManager()
-})};
+  inquirer.prompt({}).then(function (answer) {
+    startEmployeeManager();
+  });
+}
 
 function updateEmployeeRole() {
-    inquirer
-      .prompt({
-      })
-      .then(function(answer) {  
-        startEmployeeManager() 
-})};
-
-
+  inquirer.prompt({}).then(function (answer) {
+    startEmployeeManager();
+  });
+}
